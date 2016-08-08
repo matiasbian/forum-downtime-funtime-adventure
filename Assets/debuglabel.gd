ea302bc3dec8d11415e92b48e6c23aeed5138ef2
JenniBee <mcjennibee@gmail.com>
2016-08-07 23:59:35 -0400

    adding up sprites

--- Assets/debuglabel.gd
+++ Assets/debuglabel.gd
@@ -2,11 +2,12 @@ extends Label
 
 var bgZ
 var masksZ
-var mask_steeltrapZ
+var mask_chairZ
 var lightsZ
 var canvasModZ
-var mask_steeltrapZmin
-var mask_steeltrapZmax
+var mask_chairZmin
+var mask_chairZmax
+
 
 func _ready():
 	set_process(true)
@@ -14,8 +15,8 @@ func _ready():
 func _process(delta):
 	bgZ = str( get_parent().get_z() )
 	masksZ = str( get_parent().get_node("Masks").get_z() )
-	mask_steeltrapZ = str( get_parent().get_node("Masks/mask_steeltrap").get_z() )
+	mask_chairZ = str( get_parent().get_node("Masks/mask_chair").get_z() )
 	lightsZ = str( get_parent().get_node("Lights").get_z() )
-	mask_steeltrapZmin = str( get_parent().get_node("Masks/mask_steeltrap").get_z_range_min() )
-	mask_steeltrapZmax = str( get_parent().get_node("Masks/mask_steeltrap").get_z_range_max() )
-	set_text("bgZ "+bgZ+"\nmasksZ "+masksZ+"\nmask_steeltrapZ "+mask_steeltrapZ+" "+mask_steeltrapZmin+ " "+ mask_steeltrapZmax + "\nlightsZ "+lightsZ)
+	mask_chairZmin = str( get_parent().get_node("Masks/mask_chair").get_z_range_min() )
+	mask_chairZmax = str( get_parent().get_node("Masks/mask_chair").get_z_range_max() )
+	set_text("bgZ "+bgZ+"\nmasksZ "+masksZ+"\nmask_chairZ "+mask_chairZ+" "+mask_chairZmin+ " "+ mask_chairZmax + "\nlightsZ "+lightsZ)
